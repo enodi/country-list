f75f9e0fe08a8f12c2fb01ec1c545b0aeaf0a6f7
Enodi Audu <enodiaudu5@gmail.com>
2021-04-26 13:53:09 +0100

    update http domain

--- src/actions/search-countries.ts
+++ src/actions/search-countries.ts
@@ -14,7 +14,7 @@ export const searchCountryByName = async (name: string) => {
 export const currencyConverter = async (code: string, amount = '1') => {
     try {
         const response = await axios.get(
-            `http://data.fixer.io/api/convert?access_key=b9a071c8d1628b100d0517128a694ff5&from=SEK&to=${code}&amount=${amount}`
+            `https://data.fixer.io/api/convert?access_key=b9a071c8d1628b100d0517128a694ff5&from=SEK&to=${code}&amount=${amount}`
         );
         if (response.data.success) return response.data.result;
     } catch (error) {
